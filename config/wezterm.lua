7971d2668dee89f01503778799dd0aceab3e0a8d
kyrios <kyrios@tinybrain.ltd>
2024-08-10 15:21:47 +0100

    separated lua from nix

---
+++ config/wezterm.lua
@@ -0,0 +1,9 @@
+ local wezterm = require("wezterm");
+    return {
+      color_scheme = "Catppuccin Mocha",
+      default_prog = {"fish"},
+      font = wezterm.font("JetBrains Mono Nerd Font"),
+      font_size = 14.0,
+      enable_tab_bar = false,
+    }
+
--- config/wezterm.nix
+++ config/wezterm.nix
@@ -1,14 +1,5 @@
 {
   enable = true;
 
-  extraConfig = {
-    local wezterm = require("wezterm");
-    return {
-      color_scheme = "Catppuccin Mocha",
-      default_prog = {"fish"},
-      font = wezterm.font("JetBrains Mono Nerd Font"),
-      font_size = 14.0,
-      enable_tab_bar = false,
-    }
-  };
+  extraConfig = builtins.readFile ./wezterm.lua;
 }
