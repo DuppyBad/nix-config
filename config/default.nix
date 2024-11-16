ad60146bc95aabdf4265745a214d73153f641aa6
kyrios <kyrios@tinybrain.ltd>
2024-11-16 20:46:50 +0000

    damn this refactoring is hard
    inheritance is failing

--- config/default.nix
+++ config/default.nix
@@ -9,14 +9,12 @@
     ./alacritty.nix
     ./bat.nix
     ./fish.nix
-    {inherit pkgs;}
     ./foot.nix
     ./hyprland.nix
     ./kitty.nix
     ./nix-index.nix
     ./nvf.nix
     ./spicetify.nix
-    {inherit pkgs inputs;}
     ./starship.nix
     ./zoxide.nix
   ];
