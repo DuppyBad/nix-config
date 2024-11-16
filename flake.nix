433b6f0635cb2c4612a02e0c7a5c5c4833543e3b
kyrios <kyrios@tinybrain.ltd>
2024-11-16 15:36:20 +0000

    moving to default.nix structure

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       system = "x86_64-linux";
       modules = [
         # We still need the non-flake config so import it
-        ./system/configuration.nix
+        ./system
 
         # module order example
         catppuccin.nixosModules.catppuccin
