8dec6efa59beb0052b3521d624f8fd97e3ae8732
Duppy <Duppybwoy@proton.me>
2024-11-16 23:59:27 +0000

    breaking change on the way pkgs are referenced?

--- modules/system/nix/nixpkgs.nix
+++ modules/system/nix/nixpkgs.nix
@@ -3,6 +3,10 @@ _: {
     hostPlatform = "x86_64-linux";
     config = {
       allowUnfree = true;
+      # forced to add this because apple sdk is breaking ?
+      allowUnsupportedSystem = true;
+      # confused enough as things are, make things explicit
+      allowAliases = false;
     };
   };
 }
