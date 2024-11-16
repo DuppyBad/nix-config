dd53332b4273fb3b34a564513d09010760a3f8a9
kyrios <kyrios@tinybrain.ltd>
2024-11-16 17:04:47 +0000

    basic foot config done, switched to foot

--- config/foot.nix
+++ config/foot.nix
@@ -2,11 +2,14 @@
   enable = true;
   settings = {
     main = {
-      font = "Iosevka:size=14";
+      font = "Iosevka:size=13.5";
       dpi-aware = "yes";
     };
     colors = {
       alpha = 0.8;
+    };
+    cursor = {
+      style = "beam";
     };
   };
 }
