0f6609a8c8df6ce51c17e5b6ab49023ea4e4571d
Iseefa <Iseefa@proton.me>
2024-11-16 21:30:25 +0000

    place where home-manager stuff lives now

---
+++ home/programs/default.nix
@@ -0,0 +1,16 @@
+_: {
+  imports = [
+    ./alacritty.nix
+    ./bat.nix
+    ./fish.nix
+    ./foot.nix
+    ./hyprland.nix
+    ./kitty.nix
+    ./nix-index.nix
+    ./nvf.nix
+    ./spicetify.nix
+    ./starship.nix
+    ./zoxide.nix
+    ./miscpkgs.nix
+  ];
+}
