0c1760a973ceae43308c2cd43d083fd6d3eac03e
kyrios <kyrios@tinybrain.ltd>
2024-11-16 14:06:54 +0000

    changed git layout+added gpg signing

--- config/git.nix
+++ config/git.nix
@@ -1,10 +1,15 @@
 # basic configuration of git, imagine if we had our kyrios domain set properly
 {
   enable = true;
-  userName = "kyrios";
-  userEmail = "kyrios@tinybrain.ltd";
-  extraConfig = {
-    init.defaultBranch = "main";
-    safe.directory = "/home/kyrios/.config/nixos/system";
+  lfs.enable = true;
+  config = {
+    user = {
+      name = "Iseefa Francis";
+      email = "Iseefa@proton.me";
+      signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEjuzWXJAxCMhRiEvNneHD4iR8hQVR2S0pGK40ogWoWH";
+    };
+    init.defaultbranch = "main";
+    gpg.format = "ssh";
+    commit.gpgsign = "true";
   };
 }
