7bab910668231cbd7d42651f7c16723b1b6e903c
Duppy <Duppybwoy@proton.me>
2024-11-17 17:09:09 +0000

    cursor testing

--- home/default.nix
+++ home/default.nix
@@ -2,18 +2,16 @@
 # Contains the full per user configuration
 {
   home = {
-    # moving to 25.05, home manager is behind
-    enableNixpkgsReleaseCheck = false;
     username = "kyrios";
     homeDirectory = "/home/kyrios";
     # Don't you dare change the stateVersion
     stateVersion = "23.11";
-    #  pointerCursor = {
-    # gtk.enable = true;
-    # package = pkgs.catppuccin-cursors.mochaDark;
-    # size = 28;
-    # name = "catppuccin-mocha-dark-cursors";
-    #};
+    pointerCursor = {
+      gtk.enable = true;
+      package = pkgs.bibata-cursors;
+      size = 28;
+      name = "Bibata-Modern-Classic";
+    };
   };
   # colour theming stuff
   catppuccin = {
