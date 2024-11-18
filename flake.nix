b027616a8aa1380d3cd8892010b99eeac9648ace
Duppy <Duppybwoy@proton.me>
2024-11-18 00:11:37 +0000

    added 2nd host and pray

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       system = "x86_64-linux";
       modules = [
         # We still need the non-flake config so import it
-        ./hosts
+        ./hosts/sun
 
         # module order example
         catppuccin.nixosModules.catppuccin
@@ -39,8 +39,34 @@
         }
       ];
     };
-    nixosConfigurations.machina = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.moon = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      modules = [
+        # We still need the non-flake config so import it
+        ./hosts/moon
+
+        # module order example
+        catppuccin.nixosModules.catppuccin
+        inputs.spicetify-nix.nixosModules.default
+        inputs.nvf.nixosModules.default
+        # home manager as a module
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = {inherit inputs;};
+          };
+          home-manager.users.kyrios = {
+            imports = [
+              ./home
+              catppuccin.homeManagerModules.catppuccin
+              inputs.spicetify-nix.homeManagerModules.default
+              nvf.homeManagerModules.default
+            ];
+          };
+        }
+      ];
     };
   };
   inputs = {
---
+++ hosts/moon/default.nix
@@ -0,0 +1,80 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  config,
+  pkgs,
+  ...
+}: {
+  # Bootloader.
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      systemd-boot.configurationLimit = 10;
+    };
+    plymouth = {
+      enable = true;
+    };
+    initrd.systemd.enable = true;
+  };
+
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules
+  ];
+  security.pam.services.hyprlock = {};
+
+  boot.initrd.luks.devices."luks-4efa839e-2b4c-47ef-b798-c922ead1031d".device = "/dev/disk/by-uuid/4efa839e-2b4c-47ef-b798-c922ead1031d";
+
+  security.polkit.enable = true;
+  virtualisation.docker.enable = true;
+
+  networking.hostName = "moon";
+  networking.networkmanager.enable = true;
+  users.defaultUserShell = pkgs.fish;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    neovim
+    firefox
+    git
+    fish
+    wireplumber
+    acpi
+  ];
+
+  time.timeZone = "Europe/London";
+  i18n.defaultLocate = "en_GB.UTF-8";
+  console.keyMap = "uk";
+
+  environment = {
+    etc.hosts.mode = "0644";
+    variables.EDITOR = "nvim";
+  };
+
+  user.users.kyrios = {
+    isNormalUser = true;
+    home = "/home/kyrios";
+    extraGroups = ["wheel" "networkmanager" "docker"];
+    shell = pkgs.fish;
+  };
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override {fonts = ["IosevkaTerm"];})
+    iosevka
+    noto-fonts
+    noto-fonts-cjk-sans
+    cm_unicode
+  ];
+
+  programs.fish.enable = true;
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
