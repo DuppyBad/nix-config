e21aa174935fa7b11ce5a5ab7bd9aef60536c4df
kyrios <Iseefa@proton.me>
2024-12-19 00:56:06 +0000

    refactored flake, moved imports around
    how was home manager named home mamager for so long? how long?

--- flake.nix
+++ flake.nix
@@ -5,80 +5,39 @@
     self,
     nixpkgs,
     home-manager,
-    catppuccin,
-    spicetify-nix,
-    nvf,
     ...
   } @ inputs: {
-    nixosConfigurations.sun = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        # We still need the non-flake config so import it
-        ./hosts/sun
-        # module order example
-        catppuccin.nixosModules.catppuccin
-        inputs.spicetify-nix.nixosModules.default
-        inputs.nvf.nixosModules.default
-        # home manager as a module
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            extraSpecialArgs = {inherit inputs;};
-          };
-          home-manager.users.kyrios = {
-            imports = [
-              ./home
-              catppuccin.homeManagerModules.catppuccin
-              inputs.spicetify-nix.homeManagerModules.default
-              nvf.homeManagerModules.default
-            ];
-          };
-        }
-      ];
-    };
-    nixosConfigurations.moon = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        # We still need the non-flake config so import it
-        ./hosts/moon
-        # module order example
-        catppuccin.nixosModules.catppuccin
-        inputs.spicetify-nix.nixosModules.default
-        inputs.nvf.nixosModules.default
-        # home manager as a module
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            extraSpecialArgs = {inherit inputs;};
-          };
-          home-manager.users.kyrios = {
-            imports = [
-              ./home
-              catppuccin.homeManagerModules.catppuccin
-              inputs.spicetify-nix.homeManagerModules.default
-              nvf.homeManagerModules.default
-            ];
-          };
-        }
-      ];
-    };
-    nixosConfigurations.mercury = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/mercury
-        ./home
-        inputs.home-manager.nixosModules.home-manager
-      ];
+    nixosConfigurations = {
+      mercury = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/mercury
+          ./home
+          inputs.home-manager.nixosModules.home-manager
+        ];
+      };
+      sun = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/sun
+          ./home
+          inputs.home-manager.nixosModules.home-manager
+        ];
+      };
+      moon = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/moon
+        ];
+      };
     };
   };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-mamager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     catppuccin.url = "github:catppuccin/nix";
     spicetify-nix = {
