3da7a2418d85bf7eab3fc7bbff4c28d38f37b4a4
kyrios <Iseefa@proton.me>
2024-12-19 00:42:38 +0000

    refactor example on mercury

--- home/default.nix
+++ home/default.nix
@@ -1,40 +1,56 @@
-{pkgs, ...}:
+{
+  pkgs,
+  inputs,
+  ...
+}:
 # Contains the full per user configuration
 {
-  home = {
-    username = "kyrios";
-    homeDirectory = "/home/kyrios";
-    # Don't you dare change the stateVersion
-    stateVersion = "23.11";
-    pointerCursor = {
-      gtk.enable = true;
-      package = pkgs.bibata-cursors;
-      size = 28;
-      name = "Bibata-Modern-Classic";
-    };
-    # sets only usershell env
-    sessionVariables = {
-      NIXOS_OZONE_WL = "1";
-    };
-  };
-  # colour theming stuff
-  catppuccin = {
-    enable = true;
-    accent = "mauve"; #mauve is default, here for clarity
-    flavor = "mocha"; # mocha is also default
-  };
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    extraSpecialArgs = {inherit inputs;};
+    users.kyrios = {
+      imports = [
+        inputs.spicetify-nix.homeManagerModules.default
+        inputs.nvf.homeManagerModules.default
+        ./programs
+        inputs.catppuccin.homeManagerModules.catppuccin
+      ];
+      home = {
+        username = "kyrios";
+        homeDirectory = "/home/kyrios";
+        # Don't you dare change the stateVersion
+        stateVersion = "23.11";
+        pointerCursor = {
+          gtk.enable = true;
+          package = pkgs.bibata-cursors;
+          size = 28;
+          name = "Bibata-Modern-Classic";
+        };
+        # sets only usershell env
+        sessionVariables = {
+          NIXOS_OZONE_WL = "1";
+        };
+      };
+      # colour theming stuff
+      catppuccin = {
+        enable = true;
+        accent = "mauve"; #mauve is default, here for clarity
+        flavor = "mocha"; # mocha is also default
+      };
 
-  # gtk is pretty
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome-themes-extra;
-    };
-    font = {
-      name = "Iosevka";
-      size = 13;
+      # gtk is pretty
+      gtk = {
+        enable = true;
+        theme = {
+          name = "Adwaita-dark";
+          package = pkgs.gnome-themes-extra;
+        };
+        font = {
+          name = "Iosevka";
+          size = 13;
+        };
+      };
     };
   };
-  imports = [./programs];
 }
