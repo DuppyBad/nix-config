8e2ba91a9370e58f0de7b4f1edf41607954cf4ff
kyrios <Iseefa@proton.me>
2025-05-04 15:34:15 +0100

    hyprlock: removed now redundant parts

--- home/programs/hypr/hyprlock.nix
+++ home/programs/hypr/hyprlock.nix
@@ -18,29 +18,6 @@ _: {
           blur_passes = 4;
         }
       ];
-      input-field = [
-        {
-          size = "300, 50";
-          outline_thickness = 1;
-          dots_spacing = 0.3;
-          dots_center = true;
-        }
-      ];
-      label = [
-        {
-          text = "$USER, welcome back.";
-          font_family = "Iosevka";
-          font_size = 24;
-        }
-        {
-          text = "cmd[update:360000] date +'%a %b %d'";
-          font_family = "Iosevka";
-          font_size = 30;
-          position = "0,100";
-          valign = "center";
-          halign = "center";
-        }
-      ];
     };
   };
 }
