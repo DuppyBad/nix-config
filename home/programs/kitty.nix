d4d4a88f692c0a01a80e9f1b5fa6d5fd21966631
kyrios <Iseefa@proton.me>
2025-10-06 15:18:53 +0100

    feat: made kitty better with presenterm

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -3,10 +3,10 @@
     enable = true;
     font = {
       name = "IosevkaTerm Nerd Font";
-      size = 12;
+      size = 14;
     };
     settings = {
-      background_opacity = "0.8";
+      background_opacity = "1";
       confirm_os_window_close = "0";
       hide_window_decorations = "yes";
     };
