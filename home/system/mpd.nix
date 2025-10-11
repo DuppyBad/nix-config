a91272da7b70f60424f46a007f2e640ce688d893
Duppy <Duppybwoy@proton.me>
2025-10-11 16:27:16 +0100

    finalised mpd, using pipewire output

--- home/system/mpd.nix
+++ home/system/mpd.nix
@@ -2,5 +2,14 @@
   services.mpd = {
     enable = true;
     musicDirectory = "/home/kyrios/Music";
+    network.startWhenNeeded = true; #systemd only starts mpd when incoming connection to mpd socket
+    extraConfig = ''
+      follow_outside_symlinks "yes"
+      follow_inside_symlinks "yes"
+      audio_output {
+        type "pipewire"
+        name "Pipewire Output"
+      }
+    '';
   };
 }
