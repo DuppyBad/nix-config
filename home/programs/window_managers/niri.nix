341128b56694a44e5c02fd7384b7908e01f7bf09
Duppy <Duppybwoy@proton.me>
2025-10-26 11:28:25 +0000

    niri: more config, mute doesnt work

--- home/programs/window_managers/niri.nix
+++ home/programs/window_managers/niri.nix
@@ -9,6 +9,7 @@
       outputs = {
         "DP-3" = {
           mode = {
+            # can do 240 but no need
             refresh = 144.0;
             height = 2560;
             width = 1440;
@@ -18,16 +19,23 @@
             y = 0;
           };
           scale = 1;
+          # avoid weird issues where hdmi-1 is first in boot order
           focus-at-startup = true;
         };
       };
       input = {
         focus-follows-mouse.enable = true;
+        touchpad = {
+          natural-scroll = true;
+        };
         keyboard = {
           xkb = {
             layout = "gb";
           };
         };
+      };
+      animations = {
+        enable = true;
       };
       binds = with config.lib.niri.actions; {
         # General Controls that I use in all WM
@@ -41,6 +49,8 @@
         # Movement controls
         "Mod+Left".action = focus-window-up;
         "Mod+Right".action = focus-window-down;
+        # Audio control
+        "XF86AudioMute".action = spawn "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
       };
       prefer-no-csd = true;
       hotkey-overlay = {
