b30da240de0ad8149d9f811dfb0284ed38b7c2c0
Duppy <Duppybwoy@proton.me>
2025-10-27 15:16:28 +0000

    niri: maximise

--- home/programs/window_managers/niri.nix
+++ home/programs/window_managers/niri.nix
@@ -22,11 +22,14 @@
           # avoid weird issues where hdmi-1 is first in boot order
           focus-at-startup = true;
         };
+        "eDP-1" = {
+          scale = 1;
+        };
       };
       input = {
         focus-follows-mouse.enable = true;
         touchpad = {
-          natural-scroll = true;
+          natural-scroll = false;
         };
         keyboard = {
           xkb = {
@@ -43,14 +46,23 @@
         "Mod+D".action = spawn "${lib.getExe pkgs.fuzzel}";
         "Mod+Q".action = close-window;
         "Mod+F".action = fullscreen-window;
+        "Mod+Shift+F".action = maximize-column;
         "Mod+V".action = toggle-window-floating;
         "Mod+L".action = spawn "${lib.getExe pkgs.hyprlock}";
 
         # Movement controls
-        "Mod+Left".action = focus-window-up;
-        "Mod+Right".action = focus-window-down;
+        "Mod+Left".action = focus-column-left;
+        "Mod+Right".action = focus-column-right;
+        "Mod+Tab".action = focus-window-or-workspace-down;
+        "Mod+Shift+Tab".action = focus-window-or-workspace-up;
         # Audio control
-        "XF86AudioMute".action = spawn "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+        "XF86AudioMute".action = spawn ["wpctl" "set-mute" "@DEFAULT_AUDIO_SINK@" "toggle"];
+        "XF86AudioRaiseVolume".action = spawn ["wpctl" "set-volume" "-l" "1.5" "@DEFAULT_AUDIO_SINK@" "5%+"];
+        "XF86AudioLowerVolume".action = spawn ["wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "5%-"];
+
+        #Brightness Control
+        "XF86MonBrightnessUp".action = spawn ["brightnessctl" "set" "+5%"];
+        "XF86MonBrightnessDown".action = spawn ["brightnessctl" "set" "5%-"];
       };
       prefer-no-csd = true;
       hotkey-overlay = {
