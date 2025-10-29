3c49f6b3b9648b28f08c8417c66a7171b2cfc088
kyrios <Iseefa@proton.me>
2025-10-29 12:14:38 +0000

    assets: added wallpaper

--- home/programs/waybar/wlogout.nix
+++ home/programs/waybar/wlogout.nix
@@ -3,27 +3,22 @@
     enable = true;
     layout = [
       {
-        label = "lock";
         action = "hyprlock";
         text = "Lock";
       }
       {
-        label = "logout";
         action = "loginctl terminate-session";
         text = "Logout";
       }
       {
-        label = "power";
         action = "systemctl poweroff";
         text = "Shutdown";
       }
       {
-        label = "suspend";
         action = "systemctl suspend";
         text = "Bedtime";
       }
       {
-        label = "reboot";
         action = "systemctl reboot";
         text = "Reboot";
       }
