20470d56c2ab10246cea67063c4104fab61e6c11
kyrios <Iseefa@proton.me>
2025-10-30 19:06:16 +0000

    waybar: added proper labelling

--- home/programs/waybar/wlogout.nix
+++ home/programs/waybar/wlogout.nix
@@ -3,22 +3,27 @@
     enable = true;
     layout = [
       {
+        label = "lock";
         action = "hyprlock";
         text = "Lock";
       }
       {
+        label = "logout";
         action = "loginctl terminate-session";
         text = "Logout";
       }
       {
+        label = "shutdown";
         action = "systemctl poweroff";
         text = "Shutdown";
       }
       {
+        label = "suspend";
         action = "systemctl suspend";
         text = "Bedtime";
       }
       {
+        label = "reboot";
         action = "systemctl reboot";
         text = "Reboot";
       }
