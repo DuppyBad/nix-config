912c1e296b2dd4725ebcd1bbccbd2bbbd5925a46
Duppy <Duppybwoy@proton.me>
2025-10-30 23:35:06 +0000

    swww: disable hyprpaper

--- home/programs/hypr/hyprpaper.nix
+++ home/programs/hypr/hyprpaper.nix
@@ -1,6 +1,6 @@
 {
   services.hyprpaper = {
-    enable = true;
+    enable = false;
     settings = {
       preload = ["~/.config/nixos/assets/topography-mocha.png"];
       wallpaper = [", ~/.config/nixos/assets/topography-mocha.png"];
