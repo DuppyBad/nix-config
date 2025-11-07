521ec9022d33b92f9c3e56ff6722f2e6723965b1
kyrios <Iseefa@proton.me>
2025-11-07 00:53:35 +0000

    fonts: read desc
    In short, ghostty embeds Jetbrains Mono patched nerd font, which is
    unlike the standard set. I have defined a global fallback, but since it
    already has a personal set, I am unsure if it is falling back correctly.
    On the plus-side, foot falls back perfectly

--- home/programs/ghostty.nix
+++ home/programs/ghostty.nix
@@ -3,12 +3,19 @@
     enable = true;
     enableFishIntegration = true;
     settings = {
-      font-family = "Iosevka";
+      font-family = ["Geist Mono" "Symbols Nerd Font Mono"];
       font-size = 14;
-      background-opacity = 0.95;
+      background-opacity = 0.92;
       gtk-titlebar = false;
       confirm-close-surface = false;
       resize-overlay = "never";
+      #if we deny decorations, it doesn't listen to anything
+      # sometimes ghostty will latch the context of another window.
+      working-directory = "home";
+      # TODO, keep an eye on this
+      quit-after-last-window-closed = false;
+      # no terminal emulator, i dont want a toast when I copy to clipboard
+      app-notifications = false;
     };
   };
 }
